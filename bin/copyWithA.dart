e0a2ea5ff269c8f57ba828d6e81ae71b3f5aeefe
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2023-01-17 22:45:13 +0100

    Changes to be committed:

---
+++ bin/copyWithA.dart
@@ -0,0 +1,74 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+import 'package:meta/meta.dart';
+
+void main(List<String> args) {
+  final person1 = Person(age: 13, name: 'John Doe');
+  // person.age++;
+  final person1Updates = person1.copyWith(age: person1.age + 1);
+}
+
+@immutable
+class Person {
+  final String name;
+  final int age;
+  Person({
+    required this.name,
+    required this.age,
+  });
+
+  // const Person({
+  //   required this.name,
+  //   required this.age,
+  // });
+
+  // Person copyWith({
+  //   String? name,
+  //   int? age,
+  // }) {
+  //   return Person(name: name ?? this.name, age: age ?? this.age);
+  // }
+
+  Person copyWith({
+    String? name,
+    int? age,
+  }) {
+    return Person(
+      name: name ?? this.name,
+      age: age ?? this.age,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'name': name,
+      'age': age,
+    };
+  }
+
+  factory Person.fromMap(Map<String, dynamic> map) {
+    return Person(
+      name: map['name'] as String,
+      age: map['age'] as int,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Person.fromJson(String source) =>
+      Person.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() => 'Person(name: $name, age: $age)';
+
+  @override
+  bool operator ==(covariant Person other) {
+    if (identical(this, other)) return true;
+
+    return other.name == name && other.age == age;
+  }
+
+  @override
+  int get hashCode => name.hashCode ^ age.hashCode;
+}
