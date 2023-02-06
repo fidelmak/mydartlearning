7bcc640f7b8c61e1ef428b007e662b97a925e64a
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2023-02-06 17:55:29 +0100

    Changes to be committed:

--- bin/copyWithA.dart
+++ bin/copyWithA.dart
@@ -1,12 +1,18 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'dart:convert';
+//import 'dart:convert';
 
-import 'package:meta/meta.dart';
+// import 'package:meta/meta.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 
 void main(List<String> args) {
   final person1 = Person(age: 13, name: 'John Doe');
   // person.age++;
-  final person1Updates = person1.copyWith(age: person1.age + 1);
+  final person1Updates =
+      person1.copyWith(name: "Paul Fidelis ", age: person1.age + 1);
+
+  print(
+    "my name is : ${person1Updates.name} and my age is : ${person1Updates.age}",
+  );
 }
 
 @immutable
@@ -17,18 +23,6 @@ class Person {
     required this.name,
     required this.age,
   });
-
-  // const Person({
-  //   required this.name,
-  //   required this.age,
-  // });
-
-  // Person copyWith({
-  //   String? name,
-  //   int? age,
-  // }) {
-  //   return Person(name: name ?? this.name, age: age ?? this.age);
-  // }
 
   Person copyWith({
     String? name,
@@ -46,29 +40,4 @@ class Person {
       'age': age,
     };
   }
-
-  factory Person.fromMap(Map<String, dynamic> map) {
-    return Person(
-      name: map['name'] as String,
-      age: map['age'] as int,
-    );
-  }
-
-  String toJson() => json.encode(toMap());
-
-  factory Person.fromJson(String source) =>
-      Person.fromMap(json.decode(source) as Map<String, dynamic>);
-
-  @override
-  String toString() => 'Person(name: $name, age: $age)';
-
-  @override
-  bool operator ==(covariant Person other) {
-    if (identical(this, other)) return true;
-
-    return other.name == name && other.age == age;
-  }
-
-  @override
-  int get hashCode => name.hashCode ^ age.hashCode;
 }
--- bin/pdfLearn/main.dart
+++ bin/pdfLearn/main.dart
@@ -1,1 +1,43 @@
+void main(List<String> args) {
+  // this is a string declared function
+  String eatBeans() {
+    return "i cooked beans";
+  }
 
+// nesting a function to another function
+  String? newFunction() {
+    return eatBeans();
+  }
+
+  //print(newFunction());
+  // this is a positional function
+  void myEat(int x, double y, String? food) {
+    return print("$x , $y, $food ");
+  }
+
+  myEat(1, 2.5, "bread");
+// this is optional positional argument
+  void optionalMyEat(int x, double y, [String? food]) {
+    return print("$x , $y, $food ");
+  }
+
+  optionalMyEat(2, 3.5);
+// this is named optional positional argument
+  void myNameEat({int? x, double? y, String? food}) {
+    return print("$x , $y, $food ");
+  }
+
+//combine named parameters with positional parameters
+  myNameEat(x: 5, y: 4.5, food: "rice");
+
+  int myAdd(int x) {
+    return x + 10;
+  }
+
+  print(myAdd(20));
+
+  // we can program easily using arrow function
+
+  int plus(int y, int x) => y + x;
+  print(plus(4, 3));
+}
